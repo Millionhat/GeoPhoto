34db4c07562b3e37093b35ac35974bf76fb3853e
Carlos Andres Restrepo Marin <cr461204@gmail.com>
2021-11-21 18:55:57 -0500

    now opencv lib works

--- app/src/main/java/ia2/datagather/geophoto/MainActivity.java
+++ app/src/main/java/ia2/datagather/geophoto/MainActivity.java
@@ -9,11 +9,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.opencv.android.OpenCVLoader;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static String TAG ="MainActivity";
+    static{
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Funciono");
+        }else{
+            Log.d(TAG, "Noooo Funciono");
+        }
+    }
     private Button proceedBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
